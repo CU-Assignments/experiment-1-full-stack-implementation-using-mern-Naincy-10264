8289d77e831600746e72aad0619f533dc491b579
Naincy-10264 <22BCS10264@cuchd.in>
2025-02-02 12:33:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Add login functionality
+    console.log('Login submitted');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Add signup functionality
+    console.log('Signup submitted');
+});
